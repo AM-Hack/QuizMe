d0f7cdfac6dd186245dde4136819992ed4cdc77e
aadibhat09 <aadibhat09@gmail.com>
2024-03-02 13:10:46 -0800

    organization & proper file accessibility, vite still does not work

--- vite-project/src/js/gpt_test.js
+++ vite-project/src/js/gpt_test.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import { OpenAI } from 'openai';
 
-dotenv.config({ path: '.env' });
+dotenv.config({ path: '../../.env' });
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
 async function main() {

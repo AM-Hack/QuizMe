c1e361ff52f62f335010b8ee6fd2f033214697b3
Hypernova101 <92143167+Hypernova101@users.noreply.github.com>
2024-04-02 21:46:41 -0700

    add icon for light vs dark, handle clicking on icon to switch icons

--- vite-project/src/js/index.js
+++ vite-project/src/js/index.js
@@ -11,3 +11,16 @@ function SaveAndChange() {
 topic.addEventListener('keyup', () => {
     playButton.disabled = !topic.value
 })
+
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    console.log("clicked")
+    document.body.classList.toggle("dark-theme"); /* change all color things here. find the dark theme part in style.css*/
+    if (document.body.classList.contains("dark-theme")){
+        icon.src = "/src/images/light-mode.png"
+    }
+    else {
+        icon.src = "/src/images/night-mode.png"
+    }
+
+}

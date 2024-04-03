6a537ee214a281fd5a0d1d572d1be773dc3a6519
aadibhat09 <aadibhat09@gmail.com>
2024-04-02 23:35:38 -0700

    working light/dark mode

--- vite-project/src/js/index.js
+++ vite-project/src/js/index.js
@@ -1,7 +1,7 @@
 const playButton = document.querySelector('#playButton')
 const topic = document.querySelector('#topic')
 const num = document.querySelector('#qnum')
-
+document.body.classList.toggle("dark-theme")
 function SaveAndChange() {
     const topicValue = topic.value
     const numQ = num.value
@@ -19,12 +19,14 @@ topic.addEventListener('keyup', () => {
 var icon = document.getElementById("icon");
 icon.onclick = function(){
     console.log("clicked")
-    document.body.classList.toggle("dark-theme"); /* change all color things here. find the dark theme part in style.css*/
+    document.body.classList.toggle("dark-theme") /* change all color things here. find the dark theme part in style.css*/
     if (document.body.classList.contains("dark-theme")){
         icon.src = "/src/images/light-mode.png"
     }
     else {
+        document.body.classList.toggle("dark-theme")
+        document.body.classList.toggle("light-theme")
         icon.src = "/src/images/night-mode.png"
+        
     }
-
 }
